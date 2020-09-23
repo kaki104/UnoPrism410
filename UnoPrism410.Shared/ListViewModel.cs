d82fa58fc668741d7252dbaaa948e71e0f44d245
Connor Park <kaki104@hanmail.net>
2020-09-23 20:26:58 +0900

    Add project files.

---
+++ UnoPrism410.Shared/ListViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace UnoPrism410
+{
+    public class ListViewModel : BindableBase, IDialogAware
+    {
+        #region Title
+        private string _title = "Notification";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        #endregion
+
+        public IList<string> Texts { get; set; }
+
+        public ICommand TestCommand { get; set; }
+
+        public ListViewModel()
+        {
+            Texts = new List<string>
+            {
+                "Item1",
+                "Item2",
+                "Item3",
+                "Item4",
+                "Item5",
+            };
+
+            TestCommand = new DelegateCommand(OnTest);
+        }
+
+        private void OnTest()
+        {
+        }
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
---
+++ UnoPrism410.Shared/ViewModels/ShellViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace UnoPrism410.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ICommand ShowCommand { get; set; }
+
+        private IDialogService _dialogService;
+
+        public ShellViewModel(IDialogService dialogService)
+        {
+            Title = "Main Page";
+
+            ShowCommand = new DelegateCommand(OnShow);
+            _dialogService = dialogService;
+        }
+
+        private void OnShow()
+        {
+            _dialogService.ShowDialog("ListControl", null,
+                r => 
+                {
+                    if (r.Result != ButtonResult.OK) return;
+                });
+        }
+    }
+}
